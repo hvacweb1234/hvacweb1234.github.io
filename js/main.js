066dfeb555d219ed40dcf2d229ad43946e658e64
hvacweb1234 <hvacwebcraft@gmail.com>
2024-05-18 14:29:56 +0100

    Patriot HVAC services

--- js/main.js
+++ js/main.js
@@ -40,85 +40,113 @@
     });
 
 
-    // Testimonials carousel
-    $(".testimonial-carousel").owlCarousel({
+    // Modal Video
+    var $videoSrc;
+    $('.btn-play').click(function () {
+        $videoSrc = $(this).data("src");
+    });
+    console.log($videoSrc);
+    $('#videoModal').on('shown.bs.modal', function (e) {
+        $("#video").attr('src', $videoSrc + "?autoplay=1&amp;modestbranding=1&amp;showinfo=0");
+    })
+    $('#videoModal').on('hide.bs.modal', function (e) {
+        $("#video").attr('src', $videoSrc);
+    })
+
+
+    // Facts counter
+    $('[data-toggle="counter-up"]').counterUp({
+        delay: 10,
+        time: 2000
+    });
+
+
+    // Project carousel
+    $(".project-carousel").owlCarousel({
         autoplay: true,
         smartSpeed: 1000,
-        items: 1,
-        dots: false,
         loop: true,
+        center: true,
+        dots: false,
         nav: true,
         navText : [
             '<i class="bi bi-chevron-left"></i>',
             '<i class="bi bi-chevron-right"></i>'
-        ]
-    });
-
-    
-})(jQuery);
-
-//testimonial swiper
-
-var swiper = new Swiper(".testimonial-swiper", {
-    slidesPerView: 3,
-    spaceBetween: 20,
-    pagination: {
-      el: ".swiper-pagination",
-      dynamicBullets: true,
-    },
-    breakpoints: {
-      0: {
-        slidesPerView: 1,
-        spaceBetween: 20,
-      },
-      768: {
-        slidesPerView: 2,
-        spaceBetween: 30,
-      },
-      1400: {
-        slidesPerView: 3,
-        spaceBetween: 30,
-      },
-
-      
-    }
-    
-  });
- // Testimonials carousel
- $(".testimonial-carousel").owlCarousel({
-    autoplay: true,
-    smartSpeed: 1000,
-    center: true,
-    dots: false,
-    loop: true,
-    nav : true,
-    navText : [
-        '<i class="bi bi-arrow-left"></i>',
-        '<i class="bi bi-arrow-right"></i>'
-    ],
-    responsive: {
-        0:{
-            items:1
-        },
-        768:{
-            items:2
-        }
-    }
-});
-    // Dropdown on mouse hover
-    $(document).ready(function () {
-        function toggleNavbarMethod() {
-            if ($(window).width() > 992) {
-                $('.navbar .dropdown').on('mouseover', function () {
-                    $('.dropdown-toggle', this).trigger('click');
-                }).on('mouseout', function () {
-                    $('.dropdown-toggle', this).trigger('click').blur();
-                });
-            } else {
-                $('.navbar .dropdown').off('mouseover').off('mouseout');
+        ],
+        responsive: {
+            0:{
+                items:2
+            },
+            576:{
+                items:2
+            },
+            768:{
+                items:3
+            },
+            992:{
+                items:4
+            },
+            1200:{
+                items:5
             }
         }
-        toggleNavbarMethod();
-        $(window).resize(toggleNavbarMethod);
     });
+
+
+
+    
+        
+    })(jQuery);
+    
+    //testimonial swiper
+    
+    var swiper = new Swiper(".testimonial-swiper", {
+        slidesPerView: 3,
+        spaceBetween: 20,
+        pagination: {
+          el: ".swiper-pagination",
+          dynamicBullets: true,
+        },
+        breakpoints: {
+          0: {
+            slidesPerView: 1,
+            spaceBetween: 20,
+          },
+          768: {
+            slidesPerView: 2,
+            spaceBetween: 30,
+          },
+          1400: {
+            slidesPerView: 3,
+            spaceBetween: 30,
+          },
+    
+          
+        }
+        
+      });
+     // Testimonials carousel
+     $(".testimonial-carousel").owlCarousel({
+        autoplay: true,
+        smartSpeed: 1000,
+        center: true,
+        dots: false,
+        loop: true,
+        nav : true,
+        navText : [
+            '<i class="bi bi-arrow-left"></i>',
+            '<i class="bi bi-arrow-right"></i>'
+        ],
+        responsive: {
+            0:{
+                items:1
+            },
+            768:{
+                items:2
+            }
+        }
+    });
+
+    
+(jQuery);
 
